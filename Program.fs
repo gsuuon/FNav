8f900742a9922fa5233cb77504893f29e6d5e902
Steven Sun <gsuuon@gmail.com>
2023-05-03 22:36:18 -0500

    feat: scrolling and handle access errors

--- Program.fs
+++ Program.fs
@@ -46,6 +46,7 @@ let styleBreadcrumb = stext [ bg Color.SlateGray; fg Color.MintCream ]
 let styleSearchIndicator = stext [ fg Color.LightCyan ]
 let styleSelected = stext [ bg Color.Peru; fg Color.MintCream ]
 let styleUnselected = stext []
+let styleError = stext [ bg Color.Coral ]
 
 let readkey () =
     let k = Console.ReadKey(true)
@@ -54,93 +55,137 @@ let readkey () =
       modifier = k.Modifiers
       ch = k.KeyChar }
 
+let maxlines =
+    ensureAvailableRows err 10 // at least 10
+
+    let struct (col, row) = Query.getCursorPosition err
+    let height = Console.BufferHeight
+
+    height - row
+
+
 let rec pickFile mode preselect path =
-    let dirs =
-        listDirs path
-        <| match mode with
-           | Navigate -> "*"
-           | Search pat -> $"*{pat}*"
+    let returnToParent () =
+        let parent = Directory.GetParent path
 
-    err (formatPathBreadcrumbs path |> styleBreadcrumb)
-    err "\n"
+        if parent = null then
+            pickFile mode preselect path
+        else
+            let parentPath = parent.FullName
+            let thisDir = Path.GetFileName path
+            pickFile mode (Some thisDir) parentPath
 
-    match mode with
-    | Navigate -> ()
-    | Search pattern ->
-        err ((styleSearchIndicator "Search: ") + pattern)
+    let dirs' =
+        try
+            Some(
+                listDirs path
+                <| match mode with
+                   | Navigate -> "*"
+                   | Search pat -> $"*{pat}*"
+            )
+        with _ ->
+            None
+
+    match dirs' with
+    | None ->
+        err (styleError "Can't access directory")
+        Threading.Thread.Sleep 1000
+        err "\r"
+        err (Operation.eraseLine)
+        returnToParent ()
+
+    | Some dirs ->
+        err (formatPathBreadcrumbs path |> styleBreadcrumb)
         err "\n"
 
-    let rec showChoices idx =
-        let idx = Math.Max(0, Math.Min(dirs.Length - 1, idx))
-
-        dirs
-        |> Array.iteri (fun i dir ->
-            if i = idx then
-                err (styleSelected dir)
-            else
-                err (styleUnselected dir)
-
-            err sep)
-
-        err (Operation.cursorUpLines dirs.Length)
-
         match mode with
-        | Navigate ->
-            match readkey () with
-            | { key = ConsoleKey.C
-                modifier = ConsoleModifiers.Control }
-            | { key = ConsoleKey.Escape } -> Cancel
-            | { key = ConsoleKey.UpArrow }
-            | { key = ConsoleKey.K } -> showChoices (idx - 1)
-            | { key = ConsoleKey.DownArrow }
-            | { key = ConsoleKey.J } -> showChoices (idx + 1)
-            | { key = ConsoleKey.RightArrow }
-            | { key = ConsoleKey.L } -> AddPathDir dirs[idx]
-            | { key = ConsoleKey.LeftArrow }
-            | { key = ConsoleKey.H } -> RemovePathDir
-            | { key = ConsoleKey.Enter } -> Select dirs[idx]
-            | { key = ConsoleKey.Oem2 } -> ToggleMode dirs[idx]
-            | _ -> showChoices idx
+        | Navigate -> ()
+        | Search pattern ->
+            err ((styleSearchIndicator "Search: ") + pattern)
+            err "\n"
 
-        | Search pat ->
-            match readkey () with
-            | { key = ConsoleKey.Oem2 } -> ToggleMode dirs[idx]
-            | { key = ConsoleKey.Backspace } -> SearchUpdate(pat[0 .. pat.Length - 2])
-            | { key = ConsoleKey.Enter } -> Select dirs[idx]
-            | { key = ConsoleKey.UpArrow; modifier = ConsoleModifiers.Control }
-            | { key = ConsoleKey.K; modifier = ConsoleModifiers.Control } -> showChoices (idx - 1)
-            | { key = ConsoleKey.DownArrow; modifier = ConsoleModifiers.Control  }
-            | { key = ConsoleKey.J; modifier = ConsoleModifiers.Control  } -> showChoices (idx + 1)
-            | k -> SearchUpdate(pat + string k.ch)
+        let rec showChoices idx showStartIdx maxShowCount =
+            let idx = Math.Max(0, Math.Min(dirs.Length - 1, idx))
 
-    let startIdx =
-        match preselect with
-        | None -> 0
-        | Some name -> defaultArg (Array.tryFindIndex ((=) name) dirs) 0
+            let showStartIdx =
+                let showLastIdx = showStartIdx + maxShowCount
 
-    let selectAction = showChoices startIdx
+                if idx > showLastIdx then
+                    showStartIdx + idx - showLastIdx
+                else
+                    Math.Min(idx, showStartIdx)
 
-    let headerLinesCount =
-        match mode with
-        | Navigate -> 1
-        | Search _ -> 2
+            let showEndIdx = Math.Min(dirs.Length - 1, showStartIdx + maxShowCount)
+            let showCount = showEndIdx - showStartIdx
 
-    err (Operation.cursorUpLines headerLinesCount)
-    err (Operation.linesDelete <| dirs.Length + headerLinesCount)
+            [ showStartIdx..showEndIdx ]
+            |> List.map (fun i ->
+                let dir = dirs[i]
 
-    match selectAction with
-    | Cancel -> CancelPick
-    | AddPathDir dir -> addDir dir path |> pickFile mode None
-    | RemovePathDir ->
-        let parent = (Directory.GetParent path).FullName
-        let thisDir = Path.GetFileName path
-        pickFile mode (Some thisDir) parent
-    | Select dir -> addDir dir path |> PickDirectory
-    | ToggleMode lastDir ->
-        match mode with
-        | Navigate -> pickFile (Search "") (Some lastDir) path
-        | Search _ -> pickFile Navigate (Some lastDir) path
-    | SearchUpdate pat -> pickFile (Search pat) preselect path
+                Operation.eraseLine + if i = idx then styleSelected dir else styleUnselected dir)
+            |> String.concat sep
+            |> err
+
+            err (Operation.cursorUpLines showCount)
+
+            match mode with
+            | Navigate ->
+                match readkey () with
+                | { key = ConsoleKey.C
+                    modifier = ConsoleModifiers.Control }
+                | { key = ConsoleKey.Escape } -> Cancel
+                | { key = ConsoleKey.UpArrow }
+                | { key = ConsoleKey.K } -> showChoices (idx - 1) showStartIdx maxShowCount
+                | { key = ConsoleKey.DownArrow }
+                | { key = ConsoleKey.J } -> showChoices (idx + 1) showStartIdx maxShowCount
+                | { key = ConsoleKey.RightArrow }
+                | { key = ConsoleKey.L } -> AddPathDir dirs[idx]
+                | { key = ConsoleKey.LeftArrow }
+                | { key = ConsoleKey.H } -> RemovePathDir
+                | { key = ConsoleKey.Enter } -> Select dirs[idx]
+                | { key = ConsoleKey.Oem2 } -> ToggleMode dirs[idx]
+                | _ -> showChoices idx showStartIdx maxShowCount
+
+            | Search pat ->
+                match readkey () with
+                | { key = ConsoleKey.Oem2 } -> ToggleMode dirs[idx]
+                | { key = ConsoleKey.Backspace } -> SearchUpdate(pat[0 .. pat.Length - 2])
+                | { key = ConsoleKey.Enter } -> Select dirs[idx]
+                | { key = ConsoleKey.UpArrow
+                    modifier = ConsoleModifiers.Control }
+                | { key = ConsoleKey.K
+                    modifier = ConsoleModifiers.Control } -> showChoices (idx - 1) showStartIdx maxShowCount
+                | { key = ConsoleKey.DownArrow
+                    modifier = ConsoleModifiers.Control }
+                | { key = ConsoleKey.J
+                    modifier = ConsoleModifiers.Control } -> showChoices (idx + 1) showStartIdx maxShowCount
+                | k -> SearchUpdate(pat + string k.ch)
+
+        let startIdx =
+            match preselect with
+            | None -> 0
+            | Some name -> defaultArg (Array.tryFindIndex ((=) name) dirs) 0
+
+        let selectAction = showChoices startIdx 0 maxlines
+
+        let headerLinesCount =
+            match mode with
+            | Navigate -> 1
+            | Search _ -> 2
+
+        err (Operation.cursorUpLines headerLinesCount)
+        err (Operation.linesDelete <| dirs.Length + headerLinesCount)
+
+        match selectAction with
+        | Cancel -> CancelPick
+        | AddPathDir dir -> addDir dir path |> pickFile mode None
+        | RemovePathDir -> returnToParent ()
+        | Select dir -> addDir dir path |> PickDirectory
+        | ToggleMode lastDir ->
+            match mode with
+            | Navigate -> pickFile (Search "") (Some lastDir) path
+            | Search _ -> pickFile Navigate (Some lastDir) path
+        | SearchUpdate pat -> pickFile (Search pat) preselect path
 
 err (Operation.cursorHide)
 
