f30bb9d224ac34c1493e4fe77b70a3d452a0eff2
Steven Sun <gsuuon@gmail.com>
2023-05-06 23:04:15 -0500

    feat: -a accessed date, cycle sort

--- Program.fs
+++ Program.fs
@@ -12,10 +12,11 @@ let pathSep = string Path.DirectorySeparatorChar
 let usage = """Navigate through filesystem and pick a directory to stdout.
 
 Usage:
-    > fn {flags}
+    > fn {flag}
 
 Flags:
     -m          sort by modified date (defaults to sorting by name)
+    -a          sort by accessed date
 
 Example:
     > cd (fn)
@@ -25,9 +26,6 @@ let formatPathBreadcrumbs (path: string) =
     let home = Environment.SpecialFolder.UserProfile |> Environment.GetFolderPath
 
     path.Replace(home, "~").Replace(pathSep, " > ")
-
-let listDirs path pattern =
-    Directory.GetDirectories(path, pattern) |> Array.map Path.GetFileName
 
 let addDir path parent = Path.Combine [| parent; path |]
 
@@ -41,6 +39,7 @@ type SelectAction =
     | RemovePathDir
     | Select of dir: string
     | ToggleMode of wasOnDir: string
+    | CycleSort of wasOnDir: string
     | SearchUpdate of newPattern: string
     | Cancel
 
@@ -53,6 +52,7 @@ type Mode =
     | Search of pattern: string
 
 type SortMethod =
+    | ByAccessed
     | ByModified
     | ByName
 
@@ -67,6 +67,7 @@ let styleBreadcrumb = stext [ bg Color.SlateGray; fg Color.MintCream ]
 let styleSearchIndicator = stext [ fg Color.LightCyan ]
 let styleSelected = stext [ bg Color.Peru; fg Color.MintCream ]
 let styleUnselected = stext []
+let styleSort = stext [fg Color.Tan]
 let styleError = stext [ bg Color.Coral ]
 
 let readkey () =
@@ -80,6 +81,19 @@ let maxlines =
     ensureAvailableRows err 10
 
     Console.BufferHeight - 4
+
+let listDirs sort path pattern =
+    let dirs = Directory.GetDirectories(path, pattern)
+
+    match sort with
+    | ByName ->
+        dirs
+    | ByModified ->
+        dirs |> Array.sortByDescending Directory.GetLastWriteTime
+    | ByAccessed ->
+        dirs |> Array.sortByDescending Directory.GetLastAccessTime
+
+    |> Array.map Path.GetFileName
 
 // FIXME not tail-calls
 let rec pickFile opt preselect path =
@@ -97,7 +111,7 @@ let rec pickFile opt preselect path =
     let dirs' =
         try
             Some(
-                listDirs path
+                listDirs opt.sort path
                 <| match mode with
                    | Navigate -> "*"
                    | Search pat -> $"*{pat}*"
@@ -117,11 +131,21 @@ let rec pickFile opt preselect path =
         err (formatPathBreadcrumbs path |> styleBreadcrumb)
         err "\n"
 
-        match mode with
-        | Navigate -> ()
-        | Search pattern ->
-            err ((styleSearchIndicator "Search: ") + pattern)
-            err "\n"
+        let header, headerLinesCount =
+            match opt with
+            | { mode = Navigate; sort = ByName } -> "", 1
+            | { mode = Navigate } ->
+                styleSort <| sprintf "Sort (%A)" opt.sort
+                + "\n"
+                , 2
+            | { mode = Search pattern } ->
+                styleSort <| sprintf "Sort (%A)" opt.sort 
+                + (styleSearchIndicator "Search: ")
+                + pattern
+                + "\n"
+                , 2
+        
+        err header
 
         let rec showChoices idx showStartIdx maxShowCount =
             let idx = Math.Max(0, Math.Min(dirs.Length - 1, idx))
@@ -178,6 +202,11 @@ let rec pickFile opt preselect path =
               { key = ConsoleKey.Enter
                 modifier = ConsoleModifiers.Control } -> Select path
             | _, { key = ConsoleKey.Enter } -> if dirs.Length = 0 then Select path else Select dirs[idx]
+            | _, { ch = '/'; modifier = ConsoleModifiers.Alt } ->
+                if dirs.Length > 0 then
+                    CycleSort dirs[idx]
+                else
+                    CycleSort ""
             | _, { ch = '/' } ->
                 if dirs.Length > 0 then
                     ToggleMode dirs[idx]
@@ -210,10 +239,6 @@ let rec pickFile opt preselect path =
 
         let selectAction = showChoices startIdx 0 maxlines
 
-        let headerLinesCount =
-            match mode with
-            | Navigate -> 1
-            | Search _ -> 2
 
         err (Operation.cursorUpLines headerLinesCount)
         err (Operation.linesDelete <| dirs.Length + headerLinesCount)
@@ -227,10 +252,22 @@ let rec pickFile opt preselect path =
             match mode with
             | Navigate -> pickFile { opt with mode = ( Search "") }(Some lastDir) path
             | Search _ -> pickFile { opt with mode = Navigate } (Some lastDir) path
+        | CycleSort lastDir ->
+            let nextSort =
+                match opt.sort with
+                | ByName -> ByModified
+                | ByModified -> ByAccessed
+                | ByAccessed -> ByName
+            pickFile { opt with sort = nextSort } (Some lastDir) path
         | SearchUpdate pat -> pickFile { opt with mode = ( Search pat) } preselect path
 
 let opt =
     match Environment.GetCommandLineArgs() |> Array.tryItem 1 with
+    | Some "-a" -> 
+        {
+            sort = ByAccessed
+            mode = Navigate 
+        }
     | Some "-m" -> 
         {
             sort = ByModified
@@ -248,7 +285,6 @@ let opt =
             sort = ByName
             mode = Navigate 
         }
-
 
 err (Operation.cursorHide)
 
